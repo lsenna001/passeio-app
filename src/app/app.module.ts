d168e6c13985e8e3b9125be2e0c50048659fed1a
Leonardo de Sena Mesquita <leonardo.sena@trf1.jus.br>
2025-05-20 10:47:27 -0300

    Adicionando landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {provideHttpClient, withFetch} from '@angular/common/http';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {Profile} from './profile.model';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: false,
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss'
+})
+export class LandingpageComponent {
+  profile: Profile | undefined;
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  navegar(){
+    this.router.navigate(['/paginas/galeria']);
+  }
+
+  logarComGoogle() {
+
+  }
+
+  estaLogado(): boolean {
+    return !!this.profile;
+  }
+}
